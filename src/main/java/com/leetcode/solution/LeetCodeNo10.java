ef84f8ffab9f5f99ee7814177386ed1f36091f55
nahsshan <zhujunji90s@gmail.com>
2020-01-18 02:56:52 +0800

    leetcode No.12-intToRoman

--- src/main/java/com/leetcode/solution/LeetCodeNo10.java
+++ src/main/java/com/leetcode/solution/LeetCodeNo10.java
@@ -52,7 +52,26 @@ public class LeetCodeNo10 {
      * 链接：https://leetcode-cn.com/problems/regular-expression-matching
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
-    public boolean isMatch(String s, String p) {
-        return false;
+    public static boolean isMatch(String text, String pattern) {
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        dp[text.length()][pattern.length()] = true;
+
+        for (int i = text.length(); i >= 0; i--){
+            for (int j = pattern.length() - 1; j >= 0; j--){
+                boolean first_match = (i < text.length() &&
+                        (pattern.charAt(j) == text.charAt(i) ||
+                                pattern.charAt(j) == '.'));
+                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+                } else {
+                    dp[i][j] = first_match && dp[i+1][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isMatch("aaa","a*a"));
     }
 }
